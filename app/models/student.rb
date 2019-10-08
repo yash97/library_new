9bd937041da6f8038eaff1dab0869804c2b94f4a
yash97 <ythakkar97@gmail.com>
2019-10-08 02:07:39 -0400

    some changes

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,7 +3,7 @@ class Student < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :university
+  #belongs_to :university
   has_many :book_histories, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
 
@@ -12,11 +12,13 @@ class Student < ApplicationRecord
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
   validates_uniqueness_of :email, uniqueness: :message
   validates :password, presence: true, length:{minimum:6}, confirmation: true
-  
+  before_save :set_books
   education_level_options = %w(Undergraduate Masters PhD)
   validates :education_level, :inclusion => {:in => education_level_options}
 
-  # enum maximum_books: {Undergraduate: 2, Masters: 4, PhD: 6}
-  # self.max_books = maximum_books[education_level]
+  enum maximum_books: {"Undergraduate"=> 2, "Masters"=> 4, "PhD"=> 6}
+  def set_books
+    self.max_books = maximum_books[self.education_level]
+  end
   
 end
