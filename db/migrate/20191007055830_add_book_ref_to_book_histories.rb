aa1127a870aa33de9211f47cee407034fa17615d
Anmoldesai1997 <anmol20091997@gmail.com>
2019-10-09 16:54:06 -0400

    book cover

--- db/migrate/20191007055830_add_book_ref_to_book_histories.rb
+++ db/migrate/20191007055830_add_book_ref_to_book_histories.rb
@@ -1,5 +1,5 @@
 class AddBookRefToBookHistories < ActiveRecord::Migration[5.2]
   def change
-    add_reference :book_histories, :book, foreign_key: true
+    #add_reference :book_histories, :book, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_062230) do
+ActiveRecord::Schema.define(version: 2019_10_09_194913) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,21 +44,6 @@ ActiveRecord::Schema.define(version: 2019_10_09_062230) do
     t.string "name"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
-  create_table "book_histories", force: :cascade do |t|
-    t.datetime "issue_date"
-    t.datetime "return_date"
-    t.boolean "is_checked_out"
-    t.boolean "on_hold"
-    t.decimal "fine_amt"
-    t.boolean "is_approved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "student_id"
-    t.integer "book_id"
-    t.index ["book_id"], name: "index_book_histories_on_book_id"
-    t.index ["student_id"], name: "index_book_histories_on_student_id"
   end
 
   create_table "bookmarks", force: :cascade do |t|
